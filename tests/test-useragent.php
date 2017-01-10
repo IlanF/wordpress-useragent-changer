b4a85300a1c94799a60c305a47c38baff019511a
Ilan Firsov <ilanfir@gmail.com>
2017-01-10 13:03:09 +0200

    begin unit tests

---
+++ tests/test-useragent.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Firsov_User_Agent_Changer
+ */
+
+/**
+ * Sample test case.
+ */
+class Firsov_User_Agent_Changer_Unit_Test extends WP_UnitTestCase {
+
+	function test_it_sets_the_default_useragent() {
+        // save new useragent setting
+        // check that it is changed
+        
+		$this->assertTrue( true );
+	}
+    
+    function test_it_restores_wordpress_useragent() {
+        // save new useragent setting
+        // check that it changed
+        
+        // save empty useragent
+        // check that it returned to default
+        
+        $this->assertTrue( true );
+    }
+    
+}
